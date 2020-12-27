f4b9c64c7fed436142ab7fba655cd62271f6d86d
juanpfrancos <juanpanime@gmail.com>
2020-12-27 14:17:55 -0500

    Add auth API restriction

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,13 @@
-from rest_framework import generics
+from rest_framework import generics, permissions # new
 from .models import Post
 from .serializers import PostSerializer
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,) # new
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated,) # new
     queryset = Post.objects.all()
     serializer_class = PostSerializer
