d66895315aaf92943982948aef3c3bea40b60043
juanpfrancos <juanpanime@gmail.com>
2020-12-28 11:59:17 -0500

    Add title to docs.

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -4,7 +4,10 @@ from rest_framework.schemas import get_schema_view # new
 from rest_framework.documentation import include_docs_urls # new
 
 
-schema_view = get_schema_view(title='Blog API') # new
+API_TITLE = 'Blog API' # new
+API_DESCRIPTION = 'A Web API for creating and editing blog posts.' # new
+schema_view = get_schema_view(title=API_TITLE) # new
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,5 +16,5 @@ urlpatterns = [
     path('api/v1/rest-auth/', include('rest_auth.urls')),
     path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')), # new
     path('schema/', schema_view), # new
-    path('docs/', include_docs_urls(title='Blog API')), # new
+    path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)), # new
 ]
