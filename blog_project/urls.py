7175fa9c3bfb3b16f714a04d66a781838e1081fe
juanpfrancos <juanpanime@gmail.com>
2020-12-28 11:42:03 -0500

    Add schema.

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path 
+from rest_framework.schemas import get_schema_view # new
+
+
+schema_view = get_schema_view(title='Blog API') # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +11,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/rest-auth/', include('rest_auth.urls')),
     path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')), # new
+    path('schema/', schema_view), # new
 ]
